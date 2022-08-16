180279e1e2eea90621e3a335ef787a7d706273aa
Kasper Kristensen <kaskr@dtu.dk>
2022-08-16 17:00:05 +0000

    version 1.9.1

--- inst/include/tmbutils/tmbutils_extra.hpp
+++ inst/include/tmbutils/tmbutils_extra.hpp
@@ -1,13 +1,7 @@
 // Copyright (C) 2013-2015 Kasper Kristensen
 // License: GPL-2
 
-namespace tmbutils{
-// Convenience utilites
-#include "spmat.hpp"
-#include "kronecker.hpp"
-#include "matexp.hpp"
-#include "splines.hpp"
-#include "order.hpp"
+namespace tmbutils {
 
 template<class Type, class T1, class T2>
 vector<Type> dnorm(vector<Type> x, T1 mean, T2 sd, int give_log=0)
@@ -20,17 +14,13 @@ vector<Type> dnorm(vector<Type> x, T1 mean, T2 sd, int give_log=0)
 
 } // End namespace
 
-/** 
-   \brief Collection of multivariate Gaussian distributions (members listed in \ref density.hpp)
-
-   \ingroup Densities
-   
-   For use of the namespace see \ref Densities   
-*/
-namespace density{
-  using namespace tmbutils;
+// Convenience utilites
+#include "spmat.hpp"
+#include "kronecker.hpp"
+#include "matexp.hpp"
+#include "order.hpp"
+#include "splines.hpp"
+#include "interpol.hpp"
 #include "density.hpp"
-} // End namespace
-
 #include "romberg.hpp"
 #include "autodiff.hpp"
